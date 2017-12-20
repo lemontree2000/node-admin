8c440dc20d68f29d093949a2ec4c14c3851f302d
lemontree <gonelove.guo@gmail.com>
2017-12-20 21:42:56 +0800

    修改vue 静态资源的路径

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../public/admin'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/admin/',
 
     /**
      * Source Maps
